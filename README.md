a7a2bc5abcfbf6521948a1b2399a90400479273b
umair-shehzad <umairshehzad2244@gmail.com>
2022-02-23 12:08:16 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ecommerce- Building Material System
+
+> Federal Urdu University of Arts, Science & Technology, Islamabad
+> Bachelor's of Science in Computer Science (BS-CS)
+> Final Year Project
+
+## Author: Umair Shehzad
---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    BASE_URL: "http://localhost:3000",
+    MONGODB_URL:
+      "mongodb+srv://umair:Pa$$w0rd@umair.l84yj.mongodb.net/building-material-system?retryWrites=true&w=majority",
+    ACCESS_TOKEN_SECRET: ".@$*8UZj<~-9Bfn'wae2rUB/]{`G47jkd^[Ru5qd'RQ)m&5Q6",
+    REFRESH_TOKEN_SECRET: "Y3s`^y?}RL;umK<VxCF';s;vB<e9Lz-}[zmbMF7BfLSZP.#,Q:",
+    PAYPAL_CLIENT_ID:
+      "ATbX-NbsWoBFcNY-t4Zwse5wo6vZI9hqkqzc_H2iiAHtPQ-GQU8vOD_6UKSDpeA5-g26yQECqtYPmM8u",
+    CLOUD_UPDATE_PRESET: "building-material-system",
+    CLOUD_NAME: "umairshehzad",
+    CLOUD_API: "https://api.cloudinary.com/v1_1/umairshehzad/image/upload",
+  },
+};
